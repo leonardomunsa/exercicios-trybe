c0e956942d8e6fa04b804484cafd2c841115a2aa
Leonardo <leo_mc_inter@hotmail.com>
2021-05-21 02:30:49 -0300

    Exercicio 6

--- exercises/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio5.js
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio5.js
@@ -2,7 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let num;
 let number;
-for (i = 0; i < numbers.length; i += 1) {
+for (let i = 0; i < numbers.length; i += 1) {
     num = numbers[i];
     for (number of numbers) {
         if (number > num) {
