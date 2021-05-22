d30176c722abd4589d4deda006b28e000770afe2
Leonardo <leo_mc_inter@hotmail.com>
2021-05-21 23:11:49 -0300

    Exercicio bonus 3 - refazendo a logica

--- exercises/exercises/introducao-a-javascript-e-logica-de-programacao_2/bonus3.js
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_2/bonus3.js
@@ -1,9 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let multiplicados = [];
+let result;
 
-for (index = 0; index < numbers.length; index += 1) {
-    let result = numbers[index] * numbers[index + 1];
+for (index = 0; index <= numbers.length; index += 1) {
+    if (index < 9) {
+        result = numbers[index] * numbers[index+1];
+    }
     multiplicados.push(result);
 }
 
