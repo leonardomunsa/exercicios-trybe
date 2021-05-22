b7b5630bd00df69b45c3d32a98f1ef62a7c7a62c
Leonardo <leo_mc_inter@hotmail.com>
2021-05-22 01:34:26 -0300

    Criei uma array

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio1.js
@@ -0,0 +1,10 @@
+// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1, 
+// imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+
+// declarar um n
+// let n = 4;
+
+// criar um array com o número do n
+let n = [1, 2, 3, 4];
+
+//  printar os asteriscos na tela
