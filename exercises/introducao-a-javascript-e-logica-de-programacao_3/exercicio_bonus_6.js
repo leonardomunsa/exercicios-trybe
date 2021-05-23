812dbe4064019a4fee8fb9e8dd97f073b9ce8a3c
Leonardo <leo_mc_inter@hotmail.com>
2021-05-23 05:25:14 -0300

    Exercicio bonus 6 - pronto

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio_bonus_6.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio_bonus_6.js
@@ -1,6 +1,18 @@
-let numero = 7;
+let numero = 103;
 let primo = [];
 
-for (let numeroAnterior = 0; numeroAnterior < numero + 1; numeroAnterior += 1) {
+for (let numeroAnterior = 0; numeroAnterior <= numero; numeroAnterior += 1) {
+    if (numero % numeroAnterior === 0) {
+        primo.push(numeroAnterior);
+    }
+    else {
+        // do nothing
+    }
+}
 
+if (primo.length === 2) {
+    console.log('É primo');
+}
+else {
+    console.log('Não é primo');
 }
