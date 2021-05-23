8edbecc284c864b5ef4f6aa8578e3f5ff864a9ea
Leonardo <leo_mc_inter@hotmail.com>
2021-05-23 01:52:39 -0300

    Exercicio 3 - testando incluir espaços utilizando for

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio3.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio3.js
@@ -1,14 +1,16 @@
 
 // eu preciso de um código que imprima 4 linhas, sendo a primeira linha com * e 3 espaços, a segunda 2 * e 2 espaços, a terceira 3 * e 1 espaço, a última 4 *.
 // eu posso usar um array e colocar 4 itens dentro dele, sendo os itens respectivamente as linhas do triângulo invertido
+// eu posso usar uma variável string e criar um loop que vai adicionar os espaços e os asteriscos de acordo com ele
 
-let triangulo = [];
+let n = 5;
+let triangulo = '';
 
-for (let index = 1; index < 3; index += 1) {
-    for (let indox = 3; indox <= 0; indox -= 1) {
-        triangulo.push('&nbsp;&nbsp;&nbsp;*');
+for (let i = 0; i < n; i += 1) {
+    for (let a = 0; a < n; a += 1) {
+        triangulo = triangulo + ' ';
     }
-    triangulo.push('*');
+    triangulo += '*';
+    console.log(triangulo);
+    triangulo = '';
 }
-
-console.log(triangulo.join(''));
