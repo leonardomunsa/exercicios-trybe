639108ec4ae7bcc37e23b114a2b11e26aad0ee9a
Leonardo <leo_mc_inter@hotmail.com>
2021-05-24 22:34:45 -0300

    Exercicio 1 - parte 2 - criei a funçao com um array para armazenar cada letra da string

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercicio_p2_1.js
@@ -0,0 +1,13 @@
+// let string = 'james';
+
+// for (letter of string) {
+//     console.log(letter);
+// }
+
+function getString (string) {
+    let letters = [];
+    for (letter of string) {
+        letters.push(letter);
+    }
+    let palindromo = letters.join('')
+}
