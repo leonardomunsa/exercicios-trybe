7579a4733e1f2c5b612bfebae81ad2487dad40b0
Leonardo <leo_mc_inter@hotmail.com>
2021-05-27 19:42:21 -0300

    Prontinho

---
+++ bloco_5/dia_2/script2.js
@@ -0,0 +1,11 @@
+let vish = document.querySelector('#primeiroFilho');
+vish.remove();
+
+let vish2 = document.querySelector('#segundoEUltimoFilhoDoFilho');
+vish2.remove();
+
+let vish3 = document.querySelector('#terceiroFilho');
+vish3.remove();
+
+let vish4 = document.querySelector('#quartoEUltimoFilho');
+vish4.remove();
