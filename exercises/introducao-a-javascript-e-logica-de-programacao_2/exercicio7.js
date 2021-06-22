2154ebbfeaeec58ef711772243c93f9fe0b73f10
Leonardo <leo_mc_inter@hotmail.com>
2021-06-22 01:17:24 -0300

    Final

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio7.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let num;
+let number;
+for (let i = 0; i < numbers.length; i += 1) {
+    num = numbers[i];
+    for (number of numbers) {
+        if (number < num) {
+            num = number;
+        }
+    }
+}
+
+console.log(num);
