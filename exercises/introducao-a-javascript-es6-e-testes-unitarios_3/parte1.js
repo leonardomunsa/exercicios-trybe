399fcbbb37fdc4524e0b0f4ea2152ee86c7ab41c
Leonardo <leo_mc_inter@hotmail.com>
2021-06-21 23:07:15 -0300

    Parte 1 - ex 2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/parte1.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/parte1.js
@@ -9,6 +9,33 @@ function sum(a, b) {
 }
 
 // implemente seus testes aqui
-assert.strictEqual(sum(4,5), 9);
-assert.strictEqual(sum(0,0), 0);
-assert.strictEqual(sum(4,'5'), 9);
+// assert.strictEqual(sum(4,5), 9);
+// assert.strictEqual(sum(0,0), 0);
+// assert.strictEqual(sum(4,'5'), 9);
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+// assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+// assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+// const myList = [5, 6, 7, 8];
+// myRemove(myList, 5);
+// assert.deepStrictEqual(myList, [5, 6, 7, 8]);
+
+// assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+
+
+
+
+
+
+
