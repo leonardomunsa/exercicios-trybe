b2872810d7819c401bffeac45be632a6c75d75ec
Leonardo <leo_mc_inter@hotmail.com>
2021-06-21 23:59:52 -0300

    Parte 2 - ex2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/parte2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/parte2.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 // escreva a função addOne aqui
-
+/*
 function addOne(array) {
     let newArray = [];
     for (let index = 0; index < array.length; index += 1) {
@@ -17,3 +17,21 @@ const output = addOne(myArray);
 assert.strictEqual(typeof addOne, 'function');
 assert.deepStrictEqual(output, expected);
 assert.deepStrictEqual(myArray, unchanged);
+*/
+
+// escreva a função wordLengths aqui
+
+const wordLengths = (array) => {
+    let newArray = [];
+    for (let index = 0; index < array.length; index += 1) {
+        newArray.push(array[index].length);
+    }
+    return newArray;
+}
+
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+const expected = [3, 6, 10, 5];
+
+assert.strictEqual(typeof wordLengths, 'function');
+const output = wordLengths(words);
+assert.deepStrictEqual(output, expected);
