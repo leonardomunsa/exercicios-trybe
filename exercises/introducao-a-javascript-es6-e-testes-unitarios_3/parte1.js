996b091b76bdb846b1c17cfc54206b41e7c04351
Leonardo <leo_mc_inter@hotmail.com>
2021-06-21 23:14:10 -0300

    Parte 1 - ex 3

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/parte1.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/parte1.js
@@ -33,8 +33,27 @@ function myRemove(arr, item) {
 
 // assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
 
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
 
+  return arr;
+}
 
+// implemente seus testes aqui
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+const myList = [1, 2, 3];
+myRemoveWithoutCopy(myList, 3);
+assert.notDeepStrictEqual(myList, [1, 2, 3]);
+
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
 
 
 
