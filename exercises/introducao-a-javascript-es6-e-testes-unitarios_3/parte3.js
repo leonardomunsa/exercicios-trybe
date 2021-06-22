c45c5a98f4be2ace80faceabe2843989786dc7fd
Leonardo <leo_mc_inter@hotmail.com>
2021-06-22 00:39:28 -0300

    Parte 3 - ex2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/parte3.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/parte3.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 
+/*
 const greetPeople = (people) => {
   let greetingList = [];
   for (let person in people) {
@@ -14,3 +15,31 @@ const parameter = ['Irina', 'Ashleigh', 'Elsa'];
 const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
 
 assert.deepStrictEqual(greetPeople(parameter), result);
+*/
+
+const removeVowels = (word) => {
+  const characters = word.split('');
+  const results = [];
+	let number = 0;
+  for (let index = 0; index < characters.length; index += 1) {
+    if (
+      characters[index] === 'a' ||
+      characters[index] === 'o' ||
+      characters[index] === 'i' ||
+      characters[index] === 'e' ||
+      characters[index] === 'u'
+    ) {
+			number += 1;
+      results.push(number);
+    } else {
+      results.push(characters[index]);
+    }
+  }
+  return results.join('');
+};
+
+
+const parameter = 'Dayane';
+const result = 'D1y2n3';
+
+assert.strictEqual(removeVowels(parameter), result);
