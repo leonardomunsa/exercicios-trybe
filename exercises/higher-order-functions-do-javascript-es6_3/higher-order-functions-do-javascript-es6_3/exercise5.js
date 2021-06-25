64fcd04f8ccb6796cc8462e9d0a107cb27dbc4b0
Leonardo <leo_mc_inter@hotmail.com>
2021-06-25 17:19:14 -0300

    Exercício 5

--- exercises/higher-order-functions-do-javascript-es6_3/higher-order-functions-do-javascript-es6_3/exercise5.js
+++ exercises/higher-order-functions-do-javascript-es6_3/higher-order-functions-do-javascript-es6_3/exercise5.js
@@ -1,3 +1,16 @@
 const assert = require('assert');
 
 const books = require('./exercice1');
+
+const expectedResult = [
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
+];
+
+function fantasyOrScienceFictionAuthors() {
+  return books.filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia').map((book) => book.author.name).sort();
+}
+
+assert.deepStrictEqual(fantasyOrScienceFictionAuthors(), expectedResult);
