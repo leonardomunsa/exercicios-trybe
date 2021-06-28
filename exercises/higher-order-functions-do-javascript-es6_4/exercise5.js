e295a18dda3745c118b6829c6387c825c7040407
Leonardo <leo_mc_inter@hotmail.com>
2021-06-28 20:26:19 -0300

    Exercicio 5 - testando mais alternativas

--- exercises/higher-order-functions-do-javascript-es6_4/exercise5.js
+++ exercises/higher-order-functions-do-javascript-es6_4/exercise5.js
@@ -7,7 +7,9 @@ const names = [
 ];
 
 function containsA() {
-  return names.reduce(((acc, curr) => acc += curr.toLowerCase().replace(/[^a]/g, '').length), 0)
+  // return names.reduce(((acc, curr) => acc += curr.toLowerCase().replace(/[^a]/g, '').length), 0)
+  // return names.reduce(((acc, curr) => acc += curr.toLowerCase().split('a').length -1), 0)
+  return names.reduce(((acc, curr) => acc += curr.match(/a/gi).length), 0)
 }
 
 assert.deepStrictEqual(containsA(), 20);
