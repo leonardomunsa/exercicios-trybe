1d44399bd67dd85fb8ea42af0856fafdfa29694f
Leonardo <leo_mc_inter@hotmail.com>
2021-06-28 20:45:52 -0300

    fim

--- exercises/higher-order-functions-do-javascript-es6_4/exercise6.js
+++ exercises/higher-order-functions-do-javascript-es6_4/exercise6.js
@@ -1,10 +1,19 @@
 const assert = require('assert');
 
 const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
-const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+const grades = [
+  [9, 8, 10, 7, 5],
+  [10, 9, 9, 10, 8],
+  [10, 7, 10, 8, 9],
+];
 
 function studentAverage() {
-  return students.map((element, index) => ({ name: element, average: (grades[index].reduce(((acc, curr) => acc += curr), 0)) / grades[index].length}))
+  return students.map((element, index) => ({
+    name: element,
+    average:
+      grades[index].reduce((acc, curr) => (acc += curr), 0) /
+      grades[index].length,
+  }));
 }
 
 const expected = [
