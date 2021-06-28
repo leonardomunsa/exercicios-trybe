42327225f7dda2854d6426ae01540b50ccb93622
Leonardo <leo_mc_inter@hotmail.com>
2021-06-28 19:41:03 -0300

    Exercicio 4

--- exercises/higher-order-functions-do-javascript-es6_4/exercise4.js
+++ exercises/higher-order-functions-do-javascript-es6_4/exercise4.js
@@ -1,3 +1,20 @@
 const assert = require('assert');
 
 const books = require('./exercise2');
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+function longestNamedBook() {
+  return books.reduce((acc, curr) => acc.name.length > curr.name.length ? acc : curr);
+}
+
+assert.deepStrictEqual(longestNamedBook(), expectedResult);
