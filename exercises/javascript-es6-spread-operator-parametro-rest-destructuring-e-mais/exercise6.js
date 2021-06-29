ab62b843a8b6f61bef4f20c81d7749e4566f0e7c
Leonardo <leo_mc_inter@hotmail.com>
2021-06-29 13:14:56 -0300

    Exercício 7

--- exercises/javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise6.js
+++ exercises/javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise6.js
@@ -5,7 +5,7 @@ const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
 // escreva toObject abaixo
-const toObject = ([a, b, c]) => ({name: a, brand: b, year: c});
+const toObject = ([name, brand, year]) => ({name, brand, year});
 
 assert.deepStrictEqual(toObject(palio), { name: 'Palio', brand: 'Fiat', year: 2019 });
 assert.deepStrictEqual(toObject(shelbyCobra), { name: 'Shelby Cobra', brand: 'Ford', year: 1963 });
